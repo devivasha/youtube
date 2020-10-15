6d4f11be5d91e78855cc5160ea03fb4c0f48c093
Nataliia Ivaniv <nat_ivaniv@ukr.net>
2020-10-15 22:56:14 +0300

    before deploy

--- README.md
+++
@@ -1,2 +0,0 @@
-# youtube
-npm start
---
+++ youtube/src/components/SearchBar.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import PropTypes from "prop-types";
+
+
+class SearchBar extends Component {
+    state = {
+        term:''
+    };
+    onInputSearch = event =>{
+        this.setState({term: event.target.value})
+    };
+    onFormSubmit = event => {
+        event.preventDefault();
+        this.props.onTermSubmit(this.state.term);
+    };
+
+    render() {
+        return (
+            <div className="search-bar ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <label htmlFor="">Video Search</label>
+                    <input
+                        value={this.state.term}
+                        type="text"
+                        onChange={this.onInputSearch}
+                    />
+                </form>
+            </div>
+        );
+    }
+
+}
+export default SearchBar;
+SearchBar.propTypes = {
+    term: PropTypes.string,
+};
