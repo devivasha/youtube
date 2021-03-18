f8b49fbfd0d88487899c94767eba6e49504f7628
Nataliia Ivaniv <nat_ivaniv@ukr.net>
2021-03-18 10:50:13 +0200

    video

--- youtube/src/components/VideoItem.js
+++ youtube/src/components/VideoItem.js
@@ -5,7 +5,8 @@ import './VideoItem.scss';
 const VideoItem =({video, onVideoSelect})=>{
     return (
         <div onClick={()=>{onVideoSelect(video)}} className='video-item item'>
-            <img alt={video.snippet.title} className='ui image' src={video.snippet.thumbnails.medium.url}/>
+            <video src={video.snippet.thumbnails.medium.url} width="900px" height="600px"/>
+            {/*<img alt={video.snippet.title} className='ui image' src={video.snippet.thumbnails.medium.url}/>*/}
             <div className='content'>
                 <div className='header'> {video.snippet.title} </div>
             </div>
